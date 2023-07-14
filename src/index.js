ef1702abc472d9388b33aff136b9672d4f5bdbd1
David Barrio <dbarrio@gesmarket.com>
2023-07-14 10:28:00 +0200

    cuarto commit

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const router= createBrowserRouter(routes)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}>
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </Provider> */}
+    </Provider>
     <App />
   </React.StrictMode>
 );
